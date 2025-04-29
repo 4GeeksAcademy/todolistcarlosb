d1e62d84752e03fcc6777be636fbcfd563f0a056
carlossb21 <carlosandresb21@gmail.com>
2025-04-29 00:26:50 +0000

    cambios finales todolist

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,33 +1,69 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+
+	let [todos, setTodos] = useState([]);
+
+	let [texto, setTexto] = useState("");
+
+
+	let maximoTareas = 5;
+
+	const agregar = (e) => {
+		//verifica si la tecla es enter
+		if (e.key == "Enter") {
+			//valida si hay info en el input
+			if (texto == "") {
+				return;
+			}
+			//limite de tareas
+			if (todos.length < maximoTareas) {
+				todos.push(texto);
+			} else {
+				alert('solo puedes agregar maximo ' + maximoTareas + ' elementos');
+			}
+			//limpiar form
+			setTexto("");
+
+		}
+	}
+
+	const cambioTexto = (e) => {
+		setTexto(e.target.value);
+	}
+
+	const eliminarTarea = (paramIndex) => {
+		let elementos = todos.filter((item, index) => index !== paramIndex);
+		setTodos(elementos);
+	}
+
 	return (
-		<div className="text-center">
+		<div className="row justify-content-center">
+			<div className="col-md-6 col-xl-6 col-sm-12">
+				<h1 className="text-center">Todos</h1>
+				<ul className="list-group mt-4">
+					<li className="list-group-item">
+						<input type="text"
+							className="form-control"
+							placeholder="Ingrese una tarea"
+							value={texto}
+							onChange={cambioTexto}
+							onKeyDown={agregar} />
+					</li>
+					{
+						todos.map((item, index) => (
+							<li className="list-group-item tarea" key={index}>
+								{item}
+								<span className="float-end btnEliminar d-none" onClick={() => eliminarTarea(index)}>X</span>
+							</li>
+						))
+					}
+					<li className="list-group-item text-muted">Total tareas {todos.length}</li>
 
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<form>
-				<div className="row mb-3">
-					<label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
-					<div className="col-sm-10">
-						<input type="email" className="form-control" id="inputEmail3" />
-					</div>
-				</div>
-				<div className="row mb-3">
-					<label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-					<div className="col-sm-10">
-						<input type="password" className="form-control" id="inputPassword3" />
-					</div>
-				</div>
-				
-				<button type="submit" className="btn btn-primary">Sign in</button>
-			</form>
-
-
+				</ul>
+			</div>
 
 		</div>
 	);
